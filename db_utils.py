a7c54b4b6831200ed80153815e0f5e9f79f0cb2f
Will DeShazer <wdeshazer@users.noreply.github.com>
2021-01-31 23:09:31 -0500

    - creating database helper utilities to aggregate all database calls into one file. Math object will be updated later, after which all interactions with the database should reside in one file

---
+++ db_utils.py
@@ -0,0 +1,199 @@
+"""
+Equation Group Provides helper functions for pulling the LaTeX template from the equations database
+
+https://www.psycopg.org/docs/sql.html#
+"""
+
+__author__ = "William DeShazer"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+from collections import defaultdict
+from warnings import warn
+from typing import NamedTuple, Union
+from psycopg2.sql import SQL, Identifier, Placeholder, Composed
+from psycopg2 import connect, OperationalError
+from psycopg2.extras import NamedTupleCursor
+from config import config
+
+
+class NoRecordIDError(UserWarning):
+    """UserWarning for EquationGroup"""
+
+
+def get_data(table: str = None, as_columns: bool = True, verbose: bool = False):
+    """Method to pull data from database"""
+    sql = "SELECT * FROM {tbl}"
+    query = SQL(sql).format(tbl=Identifier(table))
+
+    db_params = config()
+    conn = connect(**db_params)
+
+    cur = conn.cursor(cursor_factory=NamedTupleCursor)
+
+    if verbose:
+        print('Extracting records from Table: ' + table)
+
+    try:
+        cur.execute(query)
+    except OperationalError as error:
+        print(error)
+
+    records = cur.fetchall()
+
+    if as_columns is True:
+        records = columnify(records)
+
+    cur.close()
+    conn.close()
+
+    return records
+
+
+def insert(table: str = None, data: dict = None, as_column=True, verbose: bool = False):
+    """Method to Insert New Equation Records"""
+    db_params = config()
+
+    sql = 'INSERT INTO {table} ({}) VALUES ({}) RETURNING *'
+
+    keys = data.keys()
+
+    query = SQL(sql).format(
+        SQL(', ').join(map(Identifier, keys)),
+        SQL(', ').join(map(Placeholder, keys)),
+        table=Identifier(table)
+    )
+
+    conn = connect(**db_params)
+
+    cur = conn.cursor(cursor_factory=NamedTupleCursor)
+
+    if verbose:
+        print(query.as_string(conn))
+
+    try:
+        cur.execute(query, data)
+    except OperationalError as error:
+        print(error)
+
+    conn.commit()
+
+    new_record = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    if as_column is True:
+        new_record = columnify(new_record)
+    return new_record
+
+
+def update(table: str = None, an_id: id = None, where_key: str = 'id',
+           data: dict = None, as_column: bool = True, verbose: bool = None):
+    """Insert New Record Into math_object"""
+
+    db_params = config()
+
+    if 'modified_by' not in data:
+        data.update(modified_by=db_params['user'])
+
+    fields = data.keys()
+
+    sql = "UPDATE {table} SET {fields} WHERE {pkey} = {a_value} RETURNING *"
+
+    query = SQL(sql).format(table=Identifier(table),
+                            fields=SQL(', ').join(
+                                Composed([Identifier(k), SQL(' = '), Placeholder(k)]) for k in fields
+                            ),
+                            pkey=Identifier(where_key),
+                            a_value=Placeholder('where_key')
+                            )
+
+    data.update(where_key=an_id)
+
+    conn = connect(**db_params)
+    cur = conn.cursor(cursor_factory=NamedTupleCursor)
+
+    if verbose:
+        print(query.as_string(conn))
+        print(cur.mogrify(query, data))
+
+    try:
+        cur.execute(query, data)
+    except OperationalError as error:
+        print(error)
+
+    new_record = cur.fetchall()
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    if as_column is True:
+        new_record = columnify(new_record)
+
+    return new_record
+
+
+def append(existing_records: dict = None, new_records: Union[NamedTuple, dict] = None):
+    """Append new records to existing records. Because the data is stored as a dictionary instead of
+    NamedTuples, it requires a helper function to append new data"""
+    res = existing_records
+
+    if isinstance(new_records, NamedTuple):
+        for data in new_records:
+            # noinspection PyProtectedMember
+            record: dict = data._asdict()
+            for key, value in record.items():
+                res[key].append(value)
+    elif isinstance(new_records, dict):
+        for key, value in new_records.items():
+            res[key].append(value)
+    else:
+        warn("Unrecognized type to append", UserWarning)
+
+    return res
+
+
+def record_count_total(table: str = None, verbose: bool = False) -> int:
+    """Method to get total number of eqn_groups"""
+    sql = "SELECT COUNT(id) from {table}"
+
+    query = SQL(sql).format(table=Identifier(table))
+
+    db_params = config()
+    conn = connect(**db_params)
+
+    cur = conn.cursor(cursor_factory=NamedTupleCursor)
+
+    if verbose:
+        print('Getting Count of Records in table: {table}'.format(table=table))
+
+    cur.execute(query)  # self.table))
+
+    record_count = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return record_count[0]
+
+
+def if_not_none(key: str = None, value=None):
+    """Add values to a dictionary if they are not None"""
+    data = {}
+    if value is not None:
+        data = {key: value}
+    return data
+
+
+def columnify(records):
+    """Helper function to turn NameTuple records into column-like dictionaries"""
+    res = defaultdict(list)
+
+    for data in records:
+        record: dict = data._asdict()
+        for key, value in record.items():
+            res[key].append(value)
+    return res
--- equation_group.py
+++ equation_group.py
@@ -8,12 +8,9 @@ __author__ = "William DeShazer"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-from collections import defaultdict
 from warnings import warn
-from psycopg2.sql import SQL, Identifier, Placeholder, Composed
-from psycopg2 import connect, OperationalError
-from psycopg2.extras import NamedTupleCursor
 from config import config
+from db_utils import append, get_data, insert, record_count_total, update, if_not_none
 
 
 class NoRecordIDError(UserWarning):
@@ -30,34 +27,10 @@ class EquationGroup:
 
     def get_equation_group_data(self, as_columns: bool = True, verbose: bool = False):
         """Method to pull data from database"""
-        sql = "SELECT * FROM {tbl}"
-        query = SQL(sql).format(tbl=Identifier(self.table))
+        return get_data(table=self.table, as_columns=as_columns, verbose=verbose)
 
-        db_params = config()
-        conn = connect(**db_params)
-
-        cur = conn.cursor(cursor_factory=NamedTupleCursor)
-
-        if verbose:
-            print('Extracting records from Table: ' + self.table)
-
-        try:
-            cur.execute(query)
-        except OperationalError as error:
-            print(error)
-
-        records = cur.fetchall()
-
-        if as_columns is True:
-            records = self.as_columns(records)
-
-        cur.close()
-        conn.close()
-
-        return records
-
-    def insert(self, name: str = None, notes: str = None, create_by: str = None,
-               verbose: bool = False):
+    def insert(self, name: str = None, notes: str = None,
+               created_by: str = None, as_column: bool = True, verbose: bool = False):
         """Method to Insert New Equation Records"""
         db_params = config()
 
@@ -65,51 +38,25 @@ class EquationGroup:
 
         if name is None:
             name = "{aTable} {ID:d}".format(ID=next_id, aTable=self.table)
-        if create_by is None:
-            create_by = db_params['user']
+        if created_by is None:
+            created_by = db_params['user']
 
         # data = {'name': 'New Group', 'notes': 'I hope this works', 'created_by': 'razor'}
 
-        data = {
-            'name': name,
-            'notes': notes,
-            'created_by': create_by
-        }
-
-        sql = 'INSERT INTO eqn_group ({}) VALUES ({}) RETURNING *'
-
-        keys = data.keys()
-
-        query = SQL(sql).format(
-            SQL(', ').join(map(Identifier, keys)),
-            SQL(', ').join(map(Placeholder, keys))
+        data = dict(
+            name=name,
+            notes=notes,
+            created_by=created_by
         )
 
-        conn = connect(**db_params)
+        new_records = insert(table=self.table, data=data, as_column=as_column, verbose=verbose)
 
-        cur = conn.cursor(cursor_factory=NamedTupleCursor)
-
-        if verbose:
-            print(query.as_string(conn))
-
-        try:
-            cur.execute(query, data)
-        except OperationalError as error:
-            print(error)
-
-        conn.commit()
-
-        new_record = cur.fetchall()
-
-        cur.close()
-        conn.close()
-
-        self.last_inserted = self.as_columns(new_record)
-        self.append(new_record)
+        self.last_inserted = new_records
+        self.records = append(existing_records=self.records, new_records=new_records)
 
     def update(self, an_id: id = None, where_key: str = 'id', name: str = None,
                data=None, notes: str = None, modified_by: str = None, created_by: str = None,
-               verbose: bool = None):
+               code_file_path: str = None, as_column: bool = True, verbose: bool = None):
         """Insert New Record Into math_object"""
 
         if an_id is None:
@@ -118,108 +65,23 @@ class EquationGroup:
             if data is None:
                 data = {}
 
-            db_params = config()
+            # add_field only add if there is a value available
 
-            if name is not None:
-                data.update(name=name)
-
-            if notes is not None:
-                data.update(notes=notes)
-
-            if created_by is not None:
-                data.update(created_by=created_by)
+            data.update(if_not_none('name', name))
+            data.update(if_not_none('notes', notes))
+            data.update(if_not_none('code_file_path', code_file_path))
+            data.update(if_not_none('created_by', created_by))
+            data.update(if_not_none('modified_by', modified_by))
 
             # If there is no data, then skip. Of course one could still change modified by:
-            if len(data) > 0 or modified_by is not None:
+            if len(data) > 0:
+                new_record = update(table=self.table, an_id=an_id, where_key=where_key,
+                                    as_column=as_column, verbose=verbose)
 
-                # Always require a modified by and because one can change data without specifying a modifer,
-                # this is necessary. We don't check it before the previous if, because we don't want to create
-                # a modified_by if not data was set and no modified_by was set.
-                if modified_by is None:
-                    modified_by = db_params['user']
-
-                data.update(modified_by=modified_by)
-
-                fields = data.keys()
-
-                sql = "UPDATE {table} SET {fields} WHERE {pkey} = {a_value} RETURNING *"
-
-                query = SQL(sql).format(table=Identifier(self.table),
-                                        fields=SQL(', ').join(
-                                            Composed([Identifier(k), SQL(' = '), Placeholder(k)]) for k in fields
-                                        ),
-                                        pkey=Identifier(where_key),
-                                        a_value=Placeholder('where_key')
-                                        )
-
-                data.update(where_key=an_id)
-
-                conn = connect(**db_params)
-                cur = conn.cursor(cursor_factory=NamedTupleCursor)
-
-                if verbose:
-                    print(query.as_string(conn))
-                    print(cur.mogrify(query, data))
-
-                try:
-                    cur.execute(query, data)
-                except OperationalError as error:
-                    print(error)
-
-                new_record = cur.fetchall()
-
-                conn.commit()
-
-                cur.close()
-                conn.close()
-
-                self.last_inserted = self.as_columns(new_record)
+                self.last_inserted = new_record
 
                 self.records = self.get_equation_group_data()
 
-    def append(self, new_records):
-        """Append new records to existing records. Because the data is stored as a dictionary instead of
-        NamedTuples, it requires a helper function to append new data"""
-        res = self.records
-
-        for data in new_records:
-            # noinspection PyProtectedMember
-            record: dict = data._asdict()
-            for key, value in record.items():
-                res[key].append(value)
-
-        self.records = res
-
     def record_count_total(self, verbose: bool = False) -> int:
         """Method to get total number of eqn_groups"""
-        sql = "SELECT COUNT(id) from {table}"
-
-        query = SQL(sql).format(table=Identifier(self.table))
-
-        db_params = config()
-        conn = connect(**db_params)
-
-        cur = conn.cursor(cursor_factory=NamedTupleCursor)
-
-        if verbose:
-            print('Getting Count of Records in table: {table}'.format(table=self.table))
-
-        cur.execute(query)  # self.table))
-
-        record_count = cur.fetchone()
-
-        cur.close()
-        conn.close()
-
-        return record_count[0]
-
-    @staticmethod
-    def as_columns(records):
-        """Helper function to turn NameTuple records into column-like dictionaries"""
-        res = defaultdict(list)
-
-        for data in records:
-            record: dict = data._asdict()
-            for key, value in record.items():
-                res[key].append(value)
-        return res
+        return record_count_total(table=self.table, verbose=verbose)
