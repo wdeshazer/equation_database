75f9b4cb003c8baaae3d9dbedaaa34f038e38cb0
Will DeShazer <wdeshazer@users.noreply.github.com>
2021-02-11 09:38:16 -0500

    - Add Record and Records types - Changed all type to type_name references

--- variable.py
+++ variable.py
@@ -17,12 +17,12 @@ class Variable(GroupedPhysicsObject):
         super().__init__(table_name=table_name, parent_table_name=parent_table_name)
 
     # pylint: disable=arguments-differ
-    def new_record(self, *args, unit_id: int = 1, var_type: str = 'Constant', dimensions: int = 1, **kwargs):
+    def new_record(self, *args, unit_id: int = 1, type_name: str = 'Constant', dimensions: int = 1, **kwargs):
         """Insert new Variable Record"""
-        new_record = {'variable_type': var_type}
+        new_record = {'type_name': type_name}
         super().new_record(*args, new_record=new_record, **kwargs)
 
-    def update(self, *args, var_type: str = None, **kwargs):
+    def update(self, *args, type_name: str = None, **kwargs):
         data = {}
-        data.update(add_field('var_type', var_type))
+        data.update(add_field('type_name', type_name))
         super().update(*args, data=data, **kwargs)
