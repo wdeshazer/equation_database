f5a64aafd7b26f4b6e0b171834034039bd83ea21
Will DeShazer <wdeshazer@users.noreply.github.com>
2021-02-12 08:59:33 -0500

    - Migrated equation_group.py data structure to be consistent with grouped_physics_object.py - Fixed an error in update as the result of changing id_name from a data member to a class method

--- initialize_data.py
+++ initialize_data.py
@@ -2,32 +2,39 @@
     Useful for testing and development. Shouldn't run in service"""
 import os
 from random import sample
+from pandas import DataFrame
 from equation_group import EquationGroup
 from equation import Equation
 from variable import Variable
 
+print("Importing Equation Groups")
 eq_grp = EquationGroup()
+print("Importing Equations")
 eq = Equation()
+print("Importing Variables")
 v = Variable()
 
 
-grp_count = eq_grp.record_count_total()
+grp_count = len(eq_grp.all_records)
 eq_count = len(eq.grouped_data)
 var_count = len(v.grouped_data)
 
+print("Populating Equation Groups")
 n = 6 - grp_count
 for cnt in range(n):
-    eq_grp.insert()
+    eq_grp.new_record()
 
 # Safe group id
-grp_records = eq_grp.records
+grp_records = eq_grp.all_records
 
 # Add upto 15 records and create associations for the newly generated equations
+print("Populating Equations")
 n = 16 - eq_count
 for i in range(n):
     eq.new_record()
 
 # Add upto 15 records and create associations for the newly generated equations
+print("Populating Variables")
 n = 25 - var_count
 for i in range(n):
     v.new_record()
@@ -36,17 +43,16 @@ for i in range(n):
 
 a_path = os.getcwd() + os.pathsep + 'my_py.py'
 
-for i in range(grp_count):
-    grp_id = grp_records[eq_grp.id_name][i]
+for grp_id in grp_records.index:
     eq.set_records_for_parent(parent_id=grp_id)
     used = eq.selected_data_records
     unused = eq.records_not_selected_unique
-    eq_count_in_group = len(used)
+    eq_count_in_group = len(used) if used is not None else 0
 
     # Minimum number of equations in group is 3. The following doesn't loop if eq_count_in_group > 3)
     MAX_EQ_PER_GROUP = 3
 
-    n_unused = len(unused)
+    n_unused = len(unused) if unused is not None else 0
 
     n_more_in_group = MAX_EQ_PER_GROUP - eq_count_in_group
     if n_more_in_group > 0:
@@ -55,22 +61,22 @@ for i in range(grp_count):
 
         for ind in eqns_to_insert:
             child_id = unused[ind].Index
+            print("Associating parent: " + str(grp_id) + ' with: ' + str(child_id))
             eq.associate_parent(parent_id=grp_id, child_id=child_id, code_file_path=a_path)
 
-eq_records = eq.all_records
+eq_records: DataFrame = eq.all_records
 eq_count = len(eq_records)
 
-for i in range(eq_count):
-    eq_id = eq_records[eq.id_name][i]
+for eq_id in eq_records.index:
     v.set_records_for_parent(parent_id=eq_id)
     used = v.selected_data_records
     unused = v.records_not_selected_unique
-    var_count_in_group = len(used)
+    var_count_in_group = len(used) if used is not None else 0
 
     # Minimum number of equations in group is 3. The following doesn't loop if eq_count_in_group > 3)
     MAX_VAR_PER_GROUP = 5
 
-    n_unused = len(unused)
+    n_unused = len(unused) if unused is not None else 0
 
     n_more_in_group = MAX_VAR_PER_GROUP - var_count_in_group
     if n_more_in_group > 0:
@@ -80,3 +86,4 @@ for i in range(eq_count):
         for ind in eqns_to_insert:
             child_id = unused[ind].Index
             v.associate_parent(parent_id=eq_id, child_id=child_id)
+            print("Associating parent: " + str(eq_id) + ' with: ' + str(child_id))
