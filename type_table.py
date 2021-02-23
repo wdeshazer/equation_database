9997e4447360e5da7a35c2a2dba08431e0a7129c
Will DeShazer <wdeshazer@users.noreply.github.com>
2021-02-23 08:08:24 -0500

    - Added capability to pass database connections - Added TimeLogger functionality - Created initial variable for initializing new variables

--- type_table.py
+++ type_table.py
@@ -10,12 +10,14 @@ __author__ = "William DeShazer"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+from typing import Optional
 from pandas import read_sql
 from numpy import logical_not
 from psycopg2.sql import SQL, Identifier, Literal
-from psycopg2 import connect, DatabaseError
+from psycopg2 import DatabaseError
 from psycopg2.extras import execute_values
-from config import config
+from time_logging import TimeLogger
+from db_utils import my_connect
 
 
 class NoRecordIDError(UserWarning):
@@ -25,32 +27,52 @@ class NoRecordIDError(UserWarning):
 class TypeTable:
     """Class to handle database managment of types - Includes Equations, Units and Variables"""
 
-    def __init__(self, name: str = None, verbose: bool = False):
+    def __init__(self, name: str = None, my_conn: Optional[dict] = None, t_log: Optional[TimeLogger] = None,
+                 verbose: bool = False):
         self.name = name
+        self.my_conn = my_conn
+        self.types_df = self.pull_data(my_conn=my_conn, t_log=t_log, verbose=verbose)
 
-        self.types_df = self.pull_data(verbose=verbose)
-
-    def pull_data(self, verbose: bool = False):
+    def pull_data(self, my_conn: Optional[dict] = None, t_log: Optional[TimeLogger] = None, verbose: bool = False):
         """Method to pull data from database"""
         table = self.name
+
+        if verbose is True and t_log is None:
+            t_log = TimeLogger()
 
         sql = "SELECT * FROM {tbl}"
         query = SQL(sql).format(tbl=Identifier(table))
 
-        db_params = config()
-        conn = connect(**db_params)
+        if my_conn is None:
+            my_conn = self.my_conn
+        else:
+            self.my_conn = my_conn
+
+        my_conn = my_connect(my_conn=my_conn, t_log=t_log, verbose=verbose)
+        conn = my_conn['conn']
 
         out_data = read_sql(query, con=conn, index_col='type_name')
 
         if verbose:
-            print('Extracting records from Table: ' + table)
+            t_log.new_event('Extracting records from Table: ' + table)
 
         return out_data
 
-    def insert(self, new_data, verbose: bool = False):
+    def insert(self, new_data, my_conn: Optional[dict] = None, t_log: Optional[TimeLogger] = None,
+               verbose: bool = False):
         """Method to Insert New Equation Records"""
-        db_params = config()
-        conn = connect(**db_params)
+
+        if verbose is True and t_log is None:
+            t_log = TimeLogger()
+
+        if my_conn is None:
+            my_conn = self.my_conn
+        else:
+            self.my_conn = my_conn
+
+        my_conn = my_connect(my_conn=my_conn, t_log=t_log, verbose=verbose)
+        conn = my_conn['conn']
+
         cur = conn.cursor()
 
         self.types_df = self.types_df.append(new_data)
@@ -63,6 +85,7 @@ class TypeTable:
         if verbose is True:
             print(query.as_string(cur))
             print('Values', tuples)
+            t_log.new_event('Loading: ' + self.name)
 
         try:
             execute_values(cur, query, tuples)
@@ -71,7 +94,9 @@ class TypeTable:
             print("Error: %s" % error)
             conn.rollback()
             cur.close()
-        print("execute_values() done")
+
+        if verbose is True:
+            t_log.new_event("execute_values() done")
         cur.close()
 
     def types(self):
@@ -82,29 +107,49 @@ class TypeTable:
         """Method to get total number of eqn_groups"""
         return len(self.types_df)
 
-    def reinitialize_types_df(self, verbose: bool = False):
+    def reinitialize_types_df(self, my_conn: Optional[dict] = None, t_log: Optional[TimeLogger] = None,
+                              verbose: bool = False):
         """Proper way to re-initialize types_df"""
-        self.types_df = self.pull_data(verbose=verbose)
+        if my_conn is True:
+            my_conn = self.my_conn
+        else:
+            self.my_conn = my_conn
 
-    def save_records(self, verbose: bool = False):
+        self.types_df = self.pull_data(my_conn=my_conn, t_log=t_log, verbose=verbose)
+
+    def save_records(self, my_conn: Optional[dict] = None, t_log: Optional[TimeLogger] = None, verbose: bool = False):
         """Method to store all data"""
         # find type_df values not stored.
 
-        stored_data = self.pull_data(verbose)
-        ind = logical_not(self.types_df.index.isin(stored_data.index))
-        self.insert(new_data=self.types_df[ind])
+        if my_conn is True:
+            my_conn = self.my_conn
+        else:
+            self.my_conn = my_conn
 
-    def delete_types(self, types, verbose: bool = False):
+        stored_data = self.pull_data(my_conn=my_conn, t_log=t_log, verbose=verbose)
+        ind = logical_not(self.types_df.index.isin(stored_data.index))
+        self.insert(my_conn=my_conn, t_log=t_log, new_data=self.types_df[ind])
+
+    def delete_types(self, types, my_conn: Optional[dict] = None, t_log: Optional[TimeLogger] = None,
+                     verbose: bool = False):
         """Method to Insert New Equation Records"""
-        db_params = config()
-        conn = connect(**db_params)
+        if my_conn is None:
+            my_conn = self.my_conn
+        else:
+            self.my_conn = my_conn
+
+        if verbose is True and t_log is None:
+            t_log = TimeLogger()
+
+        my_conn = my_connect(my_conn=my_conn, t_log=t_log, verbose=verbose)
+        conn = my_conn['conn']
         cur = conn.cursor()
 
-        print(type(types))
-
-        if isinstance(types, str):
-            print('you are here')
-            types = tuple([types])
+        if verbose is True:
+            t_log.new_event(type(types))
+            if isinstance(types, str):
+                print('you are here')
+                types = tuple([types])
 
         sql: str = 'DELETE FROM {table} WHERE type_name IN ({values});'
         query = SQL(sql).format(values=SQL(', ').join(map(Literal, types)),
@@ -113,7 +158,8 @@ class TypeTable:
 
         if verbose is True:
             print(query.as_string(cur))
-            print('Values', types)
+            t_log.new_event('Values' + str(types))
+            t_log.new_event('Pulling Data for: ' + self.name)
 
         try:
             cur.execute(query, types)
@@ -123,18 +169,13 @@ class TypeTable:
             conn.rollback()
             cur.close()
 
-        self.reinitialize_types_df()
-        print("execute_values() done")
+        self.reinitialize_types_df(my_conn=my_conn, t_log=t_log, verbose=verbose)
+
+        if verbose is True:
+            t_log.new_event("execute_values() done")
+
         cur.close()
 
     def index(self, type_name: str) -> int:
         """Convenience function to return the location of a type with the DataFrame"""
         return self.types_df.index.get_loc(type_name)
-
-    @staticmethod
-    def if_not_none(key: str = None, value=None):
-        """Add values to a dictionary if they are not None"""
-        data = {}
-        if value is not None:
-            data = {key: value}
-        return data
