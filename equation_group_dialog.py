e32cf70c34bb028f2853dd5710e4b89ebea19c50
Will DeShazer <wdeshazer@users.noreply.github.com>
2021-02-23 09:10:17 -0500

    - Modified to work from records

--- equation_group_dialog.py
+++ equation_group_dialog.py
@@ -9,18 +9,21 @@ __license__ = "MIT"
 # import sys
 # from PyQt5.QtCore import Qt, QSize
 # noinspection PyUnresolvedReferences
+from typing import Optional
 from PyQt5.uic import loadUi
 # from PyQt5.QtGui import QPainter, QColor, QIcon, QBrush
 from PyQt5.QtWidgets import (
-    QLineEdit, QTextEdit,
+    QApplication, QLineEdit, QTextEdit,
     QListWidget, QDialog, QDialogButtonBox
 )
 from equation_group import EquationGroup
+from equation import Equation
 
 
 class EquationGroupDialog(QDialog):
     """Equation Group Dialog"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, eqn_group: Optional[EquationGroup] = None, eqn: Optional[Equation] = None,
+                 **kwargs):
         super().__init__(*args, **kwargs)
         loadUi('eq_group_dialog.ui', self)
         # Info Widgets
@@ -33,14 +36,15 @@ class EquationGroupDialog(QDialog):
         self.new_eq_group_l_edit: QLineEdit = self.findChild(QLineEdit, 'new_eq_group_l_edit')
         self.notes_textbox: QTextEdit = self.findChild(QTextEdit, 'notes_text_edit')
 
-        self.eqn_group = EquationGroup()
-        self.populate_equation_list()
+        self.eqn_group: Optional[EquationGroup] = eqn_group
+        self.eqn: Optional[Equation] = eqn
+        self.populate_equation_group_list()
 
     def accept(self, verbose=False) -> None:
         """Accept additions to Equation Group"""
-        self.eqn_group.insert(name=self.new_eq_group_l_edit.text(),
-                              notes=self.notes_textbox.toPlainText(),
-                              verbose=verbose)
+        self.eqn_group.new_record(name=self.new_eq_group_l_edit.text(),
+                                  notes=self.notes_textbox.toPlainText(),
+                                  verbose=verbose)
         super().accept()
 
     def reject(self) -> None:
@@ -48,20 +52,41 @@ class EquationGroupDialog(QDialog):
         print('rejected')
         super().reject()
 
-    def populate_equation_list(self):
+    def populate_equation_group_list(self):
         """Populates the equation list box"""
-        records = self.get_equation_group().records
-        if records:
-            for record in records:
-                self.existing_eq_group_list.addItem(record.name)
-
-    def get_equation_group(self):
-        """Returns a reference to the equation group object"""
-        return self.eqn_group
+        records = self.eqn_group.all_records
+        for record in records.itertuples():
+            self.existing_eq_group_list.addItem(record.name)
 
     def equation_group_selected(self):
         """Updates equation group selected"""
         ind = self.existing_eq_group_list.currentIndex().row()
 
-        associated_note = self.eqn_group.records[ind].notes
+        associated_note = self.eqn_group.all_records.iloc[ind]['notes']
+        if associated_note is None:
+            associated_note = ''
         self.existing_notes_textbox.setText(associated_note)
+
+        eq_grp_id = self.eqn_group.all_records.index[ind]
+        self.eqn.set_records_for_parent(parent_id=eq_grp_id)
+        self.populate_equation_listbox()
+
+    def populate_equation_listbox(self):
+        """Populate equation listbox"""
+        eq_lb: QListWidget = self.eq_list
+        eqn = self.eqn
+
+        eq_lb.clear()
+        if eqn.selected_data_records is not None:
+            for row in eqn.selected_data_records:
+                eq_lb.addItem(row.name)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    app_eqn_group = EquationGroup()
+    app_eqn = Equation()
+    dlg = EquationGroupDialog(eqn_group=app_eqn_group, eqn=app_eqn)
+    dlg.show()
+    sys.exit(app.exec_())
